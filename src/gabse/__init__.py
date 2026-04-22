bf0fd01725df2db40b89f03a62222d6062474a56
tm-carl <carl.toller.melen@bth.se>
2026-04-22 19:23:02 +0200

    Updated version number

--- src/gabse/__init__.py
+++ src/gabse/__init__.py
@@ -8,7 +8,7 @@ This is the GABSE (Generic Agent-Based Simulation for Engineering) framework.
 # version number
 __name__ = "gabse"
 __author__ = "Carl Toller Melén"
-__version__ = "0.1.6-alpha2"
+__version__ = "0.1.6-alpha3"
 
 from .engine import Engine
 from .agent import Agent
