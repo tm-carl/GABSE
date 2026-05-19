b61b2f8de3b6695445224ba2f59b06049746e90d
tm-carl <carl.toller.melen@bth.se>
2026-05-19 15:00:40 +0200

    Updated version number.

--- src/gabse/__init__.py
+++ src/gabse/__init__.py
@@ -8,7 +8,7 @@ This is the GABSE (Generic Agent-Based Simulation for Engineering) framework.
 # version number
 __name__ = "gabse"
 __author__ = "Carl Toller Melén"
-__version__ = "0.1.6-alpha7"
+__version__ = "0.1.6"
 __email__ = ""
 __status__ = "Development"
 
